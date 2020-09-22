5a74ba56f6663056054b397ce1a76ecc635766fb
Lucas Resende <lucasresendef@hotmail.com>
2020-09-22 10:01:25 -0300

    Introduzindo JS na Calculadora

---
+++ scripts/controller/calcController.js
@@ -0,0 +1,211 @@
+class CalcController{
+
+    constructor(){
+
+        this._lastOperator = ''
+        this._lastNumber = ''
+        //EL = elemento do HTML
+        this._operation = []
+        this._displayCalcEl = document.querySelector('#display')
+        this._currentDate
+        this.initialize()
+        this.initButtonsEvents()
+
+
+    }
+
+    initialize(){
+        
+
+
+    }
+    
+    addEventListenerAll(element, events, fn){
+
+        events.split(' ').forEach(event=> {
+            element.addEventListener(event, fn, false)
+        })
+    }
+    clearAll(){
+        
+        this._operation = []
+        this._lastNumber = ''
+        this._lastOperator = ''
+        this.setLastNumberToDisplay()
+    }
+    clearEntry(){
+        this._operation.pop()
+        this.setLastNumberToDisplay()
+
+    }
+
+    getLastOperation(){
+
+       return this._operation[this._operation.length-1]
+
+    }
+
+    setLastOperation(value){
+
+       this._operation[this._operation.length-1] = value
+
+    }
+
+    isOperator(value){
+        return (['+', '-','*','%','/'].indexOf(value) > -1)
+
+    }
+
+    pushOperation(value){
+        this._operation.push(value)
+
+        if(this._operation.length > 3){
+
+            this.calc()
+          
+        }
+    }
+    calc(){
+        let last = this._operation.pop()
+        let result = eval(this._operation.join(""))
+
+        this._operation = [result, last]
+        this.setLastNumberToDisplay()
+
+    }
+    setLastNumberToDisplay(){
+        let lastNumber
+
+        for (let i = this._operation.length -1; i >= 0; i--){
+            if(!this.isOperator(this._operation[i])){
+                lastNumber = this._operation[i]
+                break
+            }
+        }
+        this.displayCalc = lastNumber
+    }
+    addOperation(value){
+
+        if(isNaN(this.getLastOperation())){
+            
+            if(this.isOperator(value)){
+                this.setLastOperation= value
+            }else if(isNaN(value)){
+
+            }else{
+                this.pushOperation(value)
+                this.setLastNumberToDisplay()
+            }
+
+        }else{
+            if(this.isOperator(value)){
+                this.pushOperation(value)
+            } else {
+                let newValue = this.getLastOperation().toString() + value.toString()
+                this.setLastOperation(parseInt(newValue))
+
+                this.setLastNumberToDisplay()
+            }
+            
+        }
+        
+
+        console.log(this._operation)
+    }
+    setError(){
+        this.displayCalc = 'Error'
+    }
+    execBtn(value){
+        switch(value){
+            case 'C':
+                this.clearAll()
+                console.log('limpa tudo')
+                break
+            case 'CE':
+                this.clearEntry()
+                console.log('limpa 1')
+                break
+            case '+':
+                console.log('+')
+                this.addOperation('+')
+                break
+            case '-':
+                this.addOperation('-')
+                break
+            case '÷':
+                this.addOperation('/')
+                break
+            case '%':
+                this.addOperation('%')
+                break
+            case '=':
+                this.addOperation('')
+                break;
+            case '¹/x':
+                this.addOperation('')
+                break
+            case 'x²':
+                this.addOperation('')
+                break
+            case '√':
+                this.addOperation('')
+                break
+            case ',':
+                this.addOperation('')
+                break
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value))
+            break
+
+            default:
+                this.setError()
+                break
+        }
+        
+    }
+    initButtonsEvents(){
+        //seleciona os botoes
+        let buttons = document.querySelectorAll('button')
+
+        //cria eventos nos botoes
+        //e = parametro da arrow function
+        buttons.forEach((btn, index)=>{
+            
+            this.addEventListenerAll(btn,'click drag', e=>{
+                let btnId = btn.id
+                this.execBtn(btnId)
+
+                
+        })
+
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e=>{
+
+                btn.style.cursor = 'pointer'
+            })
+     })
+
+    }
+
+    get displayCalc(){
+        return this._displayCalcEl.innerHTML
+    }
+    set displayCalc(valor){
+        this._displayCalcEl.innerHTML = valor
+    }
+    get currentDate(){
+        return this._currentDate
+    }
+    set currentDate(valor){
+        this._currentDate = valor
+    }
+}
